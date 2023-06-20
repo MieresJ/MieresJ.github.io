b094e51ac9c0f3864eb5d84e76641849b3ab5692
MieresJ <50970062+MieresJ@users.noreply.github.com>
2023-06-20 03:11:09 -0400

    Test

---
+++ JS/main.js
@@ -0,0 +1,23 @@
+const username = document.getElementById('username')
+const password = document.getElementById('password')
+const button = document.getElementById('button')
+
+button.addEventListener('click', (e) =>{
+    e.preventDefault()
+
+    const data={
+        username: username.value,
+        password: password.value
+    }
+
+    console.log(data)
+
+})
+
+
+$(document).ready(function(){
+    $('select').selectpicker();
+})
+// To style all selects
+
+
